47179af9004878bdc19e518abb19506a3180cfff
MissZwei <1041090303@qq.com>
2017-10-31 09:32:45 +0800

    2017-10-31

--- bxgAdmin/application/admin/controller/Index.php
+++ bxgAdmin/application/admin/controller/Index.php
@@ -3,6 +3,12 @@ namespace app\admin\controller;
 
 class Index extends \think\Controller
 {	
+    // 登录
+     public function login()
+    {
+       return $this->fetch();
+
+    }
 	// 首页框架
     public function index()
     {
@@ -14,12 +20,27 @@ class Index extends \think\Controller
     {
        return $this->fetch();
     }
+
+// 用户管理
+
     // 用户列表
     public function user_list(){
     	return $this->fetch();
     }
-    // 
+    // 用户信息
     public function user_info(){
     	return $this->fetch();
     }
+
+// 数据统计
+
+    // 用户统计
+    public function count_user(){
+        return $this->fetch();
+    }
+    // 商家统计
+    public function count_seller(){
+        return $this->fetch();
+    }
+
 }
