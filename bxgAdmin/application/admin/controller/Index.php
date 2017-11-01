17bcc2196fb69a4cc3e981173e355bff3a810578
yuanping1995 <1019979673@qq.com>
2017-11-01 10:08:55 +0800

    袁平修改

--- bxgAdmin/application/admin/controller/Index.php
+++ bxgAdmin/application/admin/controller/Index.php
@@ -1,57 +1,55 @@
 <?php
 namespace app\admin\controller;
 
-class Index extends \think\Controller
-{	
-// 登录
-     public function login()
-    {
-       return $this->fetch();
+use app\admin\model\info;
 
-    }
-// 首页框架
+class Index extends \think\Controller
+{
+	// 首页框架
     public function index()
     {
        return $this->fetch();
 
     }
-// 主页数据
+    //  
     public function index_v()
     {
        return $this->fetch();
     }
-
-// 用户管理
-
     // 用户列表
-    public function user_list(){
+    /*
+     *
+     */
+    public function user_list()
+    {
+        $list = info::with("Wallt,basic_Oder")->paginate(3)->toArray();
+        $list = $list['data'];
+        $arr = array(2,3,4,5,);
+        $i = 0;
+        foreach ($list as $key => $val)
+        {
+//            switch ($list[$key]['Upower'])
+//            {
+//                case 1:  $list[$key]['Upower'] = "普通";  break;
+//                case 2:  $list[$key]['Upower'] = "vip";   break;
+//                case 3:  $list[$key]['Upower'] = "svip";  break;
+//                default: $list[$key]['Upower'] = "ovip";
+//            }
+            $list[$key]['Upower'] =  Membership($list[$key]['Upower']);
+              $num[$key]= arraySum($val['basic_oder'], 'Total_fee',$arr);
+              Array_unshift($list[$key], $num[$key]);
+        }
+        $this->assign('arr',$list);
     	return $this->fetch();
     }
-    // 用户信息
+    //
     public function user_info(){
-    	return $this->fetch();
-    }
+        $uId['uId']  = input("id");
+        $list = info::with("honor,follow,Collection,agreement")->where($uId)->find()->toArray();
 
-// 数据统计
+        $list['Upower'] =  Membership($list['Upower']);
 
-    // 用户统计
-    public function count_user(){
+        $this->assign('arr',$list);
         return $this->fetch();
     }
-    // 商家统计
-    public function count_seller(){
-        return $this->fetch();
-    }
-
-// 订单统计
-
-    // 订单列表
-    public function order_list(){
-        return $this->fetch();
-    }
-    // 订单信息
-    public function order_detail(){
-        return $this->fetch();
-    }
-    
 }
