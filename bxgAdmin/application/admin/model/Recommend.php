d622a57a8802cfe73c8ee1846f07e43ace4e2112
yuanping1995 <1019979673@qq.com>
2017-11-01 14:19:07 +0800

    yuanping

---
+++ bxgAdmin/application/admin/model/Recommend.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/1 0001
+ * Time: 上午 11:48
+ */
+
+namespace app\admin\model;
+
+
+class Recommend
+{
+
+}
---
+++ bxgAdmin/application/api/controller/Ajaxadmin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/1 0001
+ * Time: 下午 1:36
+ */
+
+namespace app\api\controller;
+
+
+class ajaxadmin
+{
+
+}
